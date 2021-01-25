706cea8993402de87fc7354bbe2eb6a528f9f3b1
mehmetpekcan <pekcanmehmett@gmail.com>
2021-01-25 23:36:22 +0300

    statistic page created

--- app/src/main/java/com/mehmetpekcan/subscripto/StatisticsScreen.java
+++ app/src/main/java/com/mehmetpekcan/subscripto/StatisticsScreen.java
@@ -2,63 +2,63 @@ package com.mehmetpekcan.subscripto;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link StatisticsScreen#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.Map;
+import java.util.Objects;
+
 public class StatisticsScreen extends Fragment {
+ TextView totalChargeInput;
+ private FirebaseFirestore firebaseFirestore;
+ Integer totalCharge = 0;
 
- // TODO: Rename parameter arguments, choose names that match
- // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
- private static final String ARG_PARAM1 = "param1";
- private static final String ARG_PARAM2 = "param2";
-
- // TODO: Rename and change types of parameters
- private String mParam1;
- private String mParam2;
-
- public StatisticsScreen() {
-  // Required empty public constructor
- }
-
- /**
-  * Use this factory method to create a new instance of
-  * this fragment using the provided parameters.
-  *
-  * @param param1 Parameter 1.
-  * @param param2 Parameter 2.
-  * @return A new instance of fragment StatisticsScreen.
-  */
- // TODO: Rename and change types and number of parameters
- public static StatisticsScreen newInstance(String param1, String param2) {
-  StatisticsScreen fragment = new StatisticsScreen();
-  Bundle args = new Bundle();
-  args.putString(ARG_PARAM1, param1);
-  args.putString(ARG_PARAM2, param2);
-  fragment.setArguments(args);
-  return fragment;
- }
+ public StatisticsScreen() { }
 
  @Override
  public void onCreate(Bundle savedInstanceState) {
   super.onCreate(savedInstanceState);
-  if (getArguments() != null) {
-   mParam1 = getArguments().getString(ARG_PARAM1);
-   mParam2 = getArguments().getString(ARG_PARAM2);
-  }
+
+ }
+
+
+ public void onViewCreated (View view, Bundle savedInstanceState) {
+  totalChargeInput = getActivity().findViewById(R.id.totalChargeInput);
+  firebaseFirestore = FirebaseFirestore.getInstance();
+
+
+  CollectionReference colRef = firebaseFirestore.collection("subscriptions");
+  colRef.addSnapshotListener(new EventListener<QuerySnapshot>() {
+   @Override
+   public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+    if (value != null ) {
+
+     for (DocumentSnapshot snapshot: value.getDocuments()) {
+      Map<String, Object> data = snapshot.getData();
+      totalCharge += Integer.parseInt(String.valueOf(Objects.requireNonNull(data.get("price"))));
+     }
+     totalChargeInput.setText(totalCharge.toString());
+    }
+   }
+  });
+
  }
 
  @Override
  public View onCreateView(LayoutInflater inflater, ViewGroup container,
                           Bundle savedInstanceState) {
-  // Inflate the layout for this fragment
   return inflater.inflate(R.layout.fragment_statistics_screen, container, false);
  }
 }
